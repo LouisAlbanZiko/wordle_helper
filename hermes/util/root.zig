dc01865c704103a1c9dc6105da14e0487e9b4ec9
LouisAlbanZiko <louisalbanziko1610@gmail.com>
2025-11-07 09:41:25 +0100

    Removed hermes subdir

--- hermes/util/root.zig
+++
@@ -1,5 +0,0 @@
-pub const ReadBuffer = @import("ReadBuffer.zig");
-pub const template = @import("template.zig").template;
-pub const timestamp_to_iso8601 = @cImport({
-    @cInclude("time_fmt.h");
-}).timestamp_to_iso8601;
--- hermes/util/time_fmt.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <time.h>
-#include <stdio.h>
-
-size_t timestamp_to_iso8601(int64_t us, char *buffer, size_t buffer_size);
-
